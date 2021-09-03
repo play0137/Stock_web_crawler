d20613a4797e401bb73a403045db7724cc2da4b3
play0137 <play0137@yahoo.com.tw>
2021-09-03 20:16:05 +0800

    write proxies to file

--- get_free_proxies.py
+++ get_free_proxies.py
@@ -1,12 +1,19 @@
-""" Get proxies from free proxy website """
+""" Get free proxies from proxy website, and write them to file """
 from bs4 import BeautifulSoup
 import requests
+import json
+import time
+
+import global_vars
 
 response = requests.get("https://www.us-proxy.org/")
 soup = BeautifulSoup(response.text, 'lxml')
 proxies = dict()
 proxy_list = list()
-trs = soup.select("#proxylisttable tr")
+table = soup.find_all("table", class_="table table-striped table-bordered")
+trs = table.pop()
+trs = trs.select("tbody tr")
+print("the number of proxies:", len(trs))
 for tr in trs:
     tds = tr.select("td")
     if len(tds) > 6:
@@ -16,19 +23,26 @@ for tr in trs:
         ifScheme = tds[6].text
         if ifScheme == 'yes': 
             scheme = 'https'
-        else: scheme = 'http'
+        else:
+            scheme = 'http'
         proxy = "%s://%s:%s"%(scheme, ip, port)
         
         proxy = {scheme:proxy}
         try:
-            # check whther the proxy is alive or not
-            response = requests.get("http://ipv4.icanhazip.com/", proxies = proxy, timeout=10)
+            # check whether the proxy is alive or not
+            response = requests.session().get("http://ipv4.icanhazip.com/", proxies=proxy, timeout=5)
         except:
             pass
-        print(ip, response.text, sep=", ")
+        # print(ip, response.text, sep=", ")
         if ip == response.text.strip():
             proxy_list.append(proxy)
             # print(proxy)
 
-print(proxy_list)
-print("size:", len(proxy_list)) 
+print("proxy_list:", proxy_list, sep='\n')
+print("the number of valid proxies:", len(proxy_list))
+
+with open(global_vars.DIR_PATH + "proxies.txt", 'w', encoding="UTF-8") as file_w:
+    for proxy in proxy_list:
+        json.dump(proxy, file_w) # writing JSON to a file
+        file_w.write('\n')
+
