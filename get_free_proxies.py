0e4f17edef19f439020d87cc7b2d7f90cc86bd95
play0137 <play0137@yahoo.com.tw>
2021-09-12 15:27:07 +0800

    updated

--- get_free_proxies.py
+++ get_free_proxies.py
@@ -2,47 +2,50 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-import time
 
 import global_vars
 
-response = requests.get("https://www.us-proxy.org/")
-soup = BeautifulSoup(response.text, 'lxml')
-proxies = dict()
-proxy_list = list()
-table = soup.find_all("table", class_="table table-striped table-bordered")
-trs = table.pop()
-trs = trs.select("tbody tr")
-print("the number of proxies:", len(trs))
-for tr in trs:
-    tds = tr.select("td")
-    if len(tds) > 6:
-        ip = tds[0].text
-        port = tds[1].text
-        anonymity = tds[4].text
-        ifScheme = tds[6].text
-        if ifScheme == 'yes': 
-            scheme = 'https'
-        else:
-            scheme = 'http'
-        proxy = "%s://%s:%s"%(scheme, ip, port)
-        
-        proxy = {scheme:proxy}
-        try:
-            # check whether the proxy is alive or not
-            response = requests.session().get("http://ipv4.icanhazip.com/", proxies=proxy, timeout=5)
-        except:
-            pass
-        # print(ip, response.text, sep=", ")
-        if ip == response.text.strip():
-            proxy_list.append(proxy)
-            # print(proxy)
+def main():
+    response = requests.get("https://www.us-proxy.org/")
+    soup = BeautifulSoup(response.text, 'lxml')
+    proxy_list = list()
+    table = soup.find_all("table", class_="table table-striped table-bordered")
+    trs = table.pop()
+    trs = trs.select("tbody tr")
+    print("the number of proxies:", len(trs))
+    for tr in trs:
+        tds = tr.select("td")
+        if len(tds) > 6:
+            ip = tds[0].text
+            port = tds[1].text
+            ifScheme = tds[6].text
+            if ifScheme == 'yes': 
+                scheme = 'https'
+            else:
+                scheme = 'http'
+            proxy = "%s://%s:%s"%(scheme, ip, port)
+            
+            proxy = {scheme:proxy}
+            try:
+                # check whether the proxy is alive or not
+                response = requests.session().get("http://ipv4.icanhazip.com/", proxies=proxy, timeout=5)
+            except requests.exceptions.Timeout as timeout_err:
+                print("Timeout error:", timeout_err)
+            except Exception as e:
+                print(e)
+            # print(ip, response.text, sep=", ")
+            if ip == response.text.strip():
+                print(proxy, "is ok")
+                proxy_list.append(proxy)
+    
+    print("proxy_list:", proxy_list, sep='\n')
+    print("the number of valid proxies:", len(proxy_list))
+    
+    with open(global_vars.DIR_PATH + "proxies.txt", 'w', encoding="UTF-8") as file_w:
+        for proxy in proxy_list:
+            json.dump(proxy, file_w) # writing JSON to a file
+            file_w.write('\n')
 
-print("proxy_list:", proxy_list, sep='\n')
-print("the number of valid proxies:", len(proxy_list))
 
-with open(global_vars.DIR_PATH + "proxies.txt", 'w', encoding="UTF-8") as file_w:
-    for proxy in proxy_list:
-        json.dump(proxy, file_w) # writing JSON to a file
-        file_w.write('\n')
-
+if __name__ == "__main__":
+    main()
