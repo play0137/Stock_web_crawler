a3c26159ab547d56fcfb84a601b93d877cb6fb1e
Ajin <play0137@gmail.com>
2022-01-11 15:10:50 +0800

    update

--- stock_filter_df.py
+++ stock_filter_df.py
@@ -71,7 +71,7 @@ def main():
         last_month = datetime.now().month-1
         if last_month <= 0:
             last_month = 12
-        df = pd.read_excel(file_path, sheet_name=f"{datetime.now().month-1}月")
+        df = pd.read_excel(file_path, sheet_name=f"{last_month}月")
     except ValueError as ve:
         print("ValueError:", ve)
         sys.stderr.write("Please excute stock_web_crawler.py first.\n")
